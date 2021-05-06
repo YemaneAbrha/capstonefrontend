b3f0a31d869cdfe60522a0c0fbffad3f25e06f37
yemane <yemaneabrha21@gmail.com>
2021-05-06 11:32:05 +0300

    Navigation Drawar Added

--- src/components/VerticalNav.vue
+++ src/components/VerticalNav.vue
@@ -1,43 +1,62 @@
 <template>
-        <v-app-bar
-      app
-      color="primary"
-      dark
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant.sync="mini"
+      permanent
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+      <v-list-item class="px-2">
+      <v-list-item-title>Yemane Abrha</v-list-item-title>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+        <v-btn
+          icon
+          @click.stop="mini = !mini"
+        >
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+      </v-list-item>
 
-      <v-spacer></v-spacer>
+      <v-divider></v-divider>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+      <v-list >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+       <template v-slot:append>
+        <div class="pa-2">
+          <v-btn block>
+            Logout
+          </v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
 </template>
 <script>
-export default {
-    
-}
+  export default {
+    data () {
+      return {
+        drawer: true,
+        items: [
+          { title: 'Dash-Board', icon: 'mdi-view-dashboard' },
+          { title: 'Maintenance', icon: 'mdi-account' },
+          { title: 'Bill', icon: 'mdi-currency-usd' },
+          { title: 'Properties', icon: 'mdi-home-city' },
+          { title: 'User', icon: 'mdi-account-group' },
+          { title: 'Message', icon: 'mdi-message' },
+          
+        ],
+        mini: false,
+      }
+    },
+  }
 </script>
