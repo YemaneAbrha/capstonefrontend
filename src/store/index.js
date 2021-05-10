3f3cd7880ec3c0202f9f05c7205890f4cb5e33d5
YemaneAbrha <yemaneabrha21@gmail.com>
2021-05-10 09:46:13 +0300

    Bill and Property Added

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,29 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+/*
+ * @import  data from 
+    the model.
+ */
+import bill from "../data/bill";
+import maintenance from "../data/maintenance";
+import property from "../data/properties";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    maintenance,
+    bill,
+    property,
   },
   mutations: {
+    addBill(state, payload) {
+      state.bill.push(payload);
+    },
+    registerProperty(state, payload) {
+      state.property.push(payload);
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
