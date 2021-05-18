2c7b67de5adb406aff3abf9f389febf3e6c20914
YemaneAbrha <yemaneabrha21@gmail.com>
2021-05-18 13:41:05 +0300

    Card Added

--- src/views/CreateBill.vue
+++ src/views/CreateBill.vue
@@ -2,46 +2,57 @@
   <v-sheet height="sheetHeight">
     <v-container class="fill-hight">
       <v-container ma-5></v-container>
-      <v-form v-model="valid">
-        <v-container>
-          <v-row>
-            <v-col cols="12" md="4" offset-sm="1" offset-md="2">
-              <v-text-field v-model="title" :rules="titleRules" label="Title *" required></v-text-field>
-            </v-col>
+      <v-card class="mx-auto" outlined>
+        <v-form v-model="valid">
+          <v-container>
+            <v-row>
+              <v-col cols="12" md="4" offset-sm="1" offset-md="2">
+                <v-text-field v-model="title" :rules="titleRules" label="Title *" required></v-text-field>
+              </v-col>
 
-            <v-col cols="12" md="4" offset-sm="1" offset-md="2">
-              <v-text-field v-model="amount" :rules="amountRules" label="Amount *" required></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="12" md="4" offset-sm="1" offset-md="2">
-              <v-textarea v-model="description" label="Description" :rules="descriptionRule"></v-textarea>
-            </v-col>
-            <v-col cols="12" md="4" offset-sm="1" offset-md="2">
-              <v-dialog
-                ref="dialog"
-                v-model="modal"
-                :return-value.sync="date"
-                persistent
-                width="290px"
-              >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field v-model="date" label="Due-Date" readonly v-bind="attrs" v-on="on"></v-text-field>
-                </template>
-                <v-date-picker v-model="date" scrollable>
-                  <v-btn text color="primary" @click="modal = false">Cancel</v-btn>
-                  <v-btn text color="primary" @click="$refs.dialog.save(date)">OK</v-btn>
-                </v-date-picker>
-              </v-dialog>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="12" md="8" offset-sm="1" offset-md="2">
-              <v-btn block color="success" class="ml-10" @click="createBill()" outlined large>Submit</v-btn>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-form>
+              <v-col cols="12" md="4" offset-sm="1" offset-md="2">
+                <v-text-field v-model="amount" :rules="amountRules" label="Amount *" required></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12" md="4" offset-sm="1" offset-md="2">
+                <v-textarea v-model="description" label="Description" :rules="descriptionRule"></v-textarea>
+              </v-col>
+              <v-col cols="12" md="4" offset-sm="1" offset-md="2">
+                <v-dialog
+                  ref="dialog"
+                  v-model="modal"
+                  :return-value.sync="date"
+                  persistent
+                  width="290px"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field v-model="date" label="Due-Date" readonly v-bind="attrs" v-on="on"></v-text-field>
+                  </template>
+                  <v-date-picker v-model="date" scrollable>
+                    <v-btn text color="primary" @click="modal = false">Cancel</v-btn>
+                    <v-btn text color="primary" @click="$refs.dialog.save(date)">OK</v-btn>
+                  </v-date-picker>
+                </v-dialog>
+                <v-spacer></v-spacer>
+                <v-file-input show-size label="Bill Document" />
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12" md="8" offset-sm="1" offset-md="2">
+                <v-btn
+                  block
+                  color="success"
+                  class="ml-10"
+                  @click="createBill()"
+                  outlined
+                  large
+                >Submit</v-btn>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-form>
+      </v-card>
     </v-container>
   </v-sheet>
 </template>
