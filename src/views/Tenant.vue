7fc39cff169a3e746a8d57f33a4f205d9202dc4a
YemaneAbrha <yemaneabrha21@gmail.com>
2021-05-19 09:14:12 +0300

    New Tenant Table Added with Action

--- src/views/Tenant.vue
+++ src/views/Tenant.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-sheet>
+  <v-container>
     <v-row align="center" justify="center">
       <v-container ma-5>
         <h3 class="text-center display-3 hidden-xs-only">New Registered Tenant</h3>
@@ -7,60 +7,117 @@
       </v-container>
     </v-row>
     <v-row align="center" justify="center">
-      <v-data-table
-        :headers="headers"
-        :items="newRegistedUser"
-        :items-per-page="3"
-        class="elevation-1"
-      ></v-data-table>
+      <v-data-table :headers="headers" :items="newRegistedUser" class="elevation-1">
+        <template v-slot:top>
+          <v-toolbar flat>
+            <v-divider class="mx-4" inset vertical></v-divider>
+            <v-spacer></v-spacer>
+            <v-dialog v-model="dialogDelete" max-width="500px">
+              <v-card>
+                <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                  <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                  <v-spacer></v-spacer>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-toolbar>
+        </template>
+        <template v-slot:item.actions="{ item }">
+          <v-icon @click="paidFee(item)" color="success">mdi-check</v-icon>
+          <v-icon small @click="deleteItem(item)" color="error">mdi-delete</v-icon>
+        </template>
+        <template v-slot:no-data>No Data</template>
+      </v-data-table>
     </v-row>
-    <v-row align="center" justify="center">
-      <v-container ma-5>
-        <h3 class="text-center display-3 hidden-xs-only">Rooms</h3>
-        <h3 class="text-center display-1 hidden-sm-and-up">Rooms</h3>
-      </v-container>
-    </v-row>
-    <v-row align="center" justify="center">
-      <v-data-table
-        :headers="headers"
-        :items="newMaintenance"
-        :items-per-page="3"
-        class="elevation-1"
-      ></v-data-table>
-    </v-row>
-  </v-sheet>
+  </v-container>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      headers: [
-        {
-          text: "Full Name",
-          align: "start",
-          sortable: false,
-          value: "fullname"
-        },
-        { text: "Phone Number", value: "phonenumber", sortable: false },
-        { text: "Email", value: "email", sortable: false },
-        { text: "Date ", value: "date", sortable: true },
-        { text: "Actions", value: "actions", sortable: false }
-      ],
-      newRegistedUser: [
-        {
-          fullname: "Abebe Zegeye",
-          phonenumber: "+25121314159",
-          email: "abebe@gmail.com",
-          date: "24-Tus-2020"
-        },
-        {
-          fullname: "Abebe Zegeye",
-          phonenumber: "+25121314159",
-          email: "abebe@gmail.com",
-          date: "24-Tus-2020"
-        }
-      ]
-    };
+  data: () => ({
+    dialogDelete: false,
+    headers: [
+      {
+        text: "Full Name",
+        align: "start",
+        sortable: false,
+        value: "fullname"
+      },
+      { text: "Phone Number", value: "phonenumber", sortable: false },
+      { text: "Email", value: "email", sortable: false },
+      { text: "Date ", value: "date", sortable: true },
+      { text: "Actions", value: "actions", sortable: false }
+    ],
+    newRegistedUser: [
+      {
+        fullname: "Abebe Zegeye",
+        phonenumber: "+25121314159",
+        email: "abebe@gmail.com",
+        date: "24-Tus-2020"
+      },
+      {
+        fullname: "Abebe Zegeye",
+        phonenumber: "+25121314159",
+        email: "abebe@gmail.com",
+        date: "22-Tus-2020"
+      }
+    ]
+  }),
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    },
+    dialogDelete(val) {
+      val || this.closeDelete();
+    }
+  },
+  methods: {
+    paidFee(item) {
+      this.editedIndex = this.$store.state.pendingBill.indexOf(item);
+      console.log(item);
+      this.$store.commit("addPaidFee", {
+        item: item,
+        index: this.editedIndex
+      });
+    },
+    deleteItem(item) {
+      this.editedIndex = this.$store.state.pendingBill.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialogDelete = true;
+    },
+
+    deleteItemConfirm() {
+      this.$store.commit("deletePendingBill", {
+        index: this.editedIndex
+      });
+      this.closeDelete();
+    },
+
+    close() {
+      // this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    closeDelete() {
+      this.dialogDelete = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    save() {
+      this.$store.commit("editBill", {
+        item: this.editedItem
+      });
+      this.close();
+    }
   }
 };
 </script>
