254151634ae40ab5954f1e173914eb6cc4ae537b
YemaneAbrha <yemaneabrha21@gmail.com>
2021-05-21 09:28:12 +0300

    Data Table Added

--- src/views/Tenant.vue
+++ src/views/Tenant.vue
@@ -7,23 +7,25 @@
       </v-container>
     </v-row>
     <v-row align="center" justify="center">
-      <v-data-table :headers="headers" :items="newRegistedUser" class="elevation-1">
+      <v-data-table
+        :headers="headers"
+        :items="$store.state.newRegisteredTenant"
+        class="elevation-1"
+      >
         <template v-slot:top>
-          <v-toolbar flat>
-            <v-divider class="mx-4" inset vertical></v-divider>
-            <v-spacer></v-spacer>
-            <v-dialog v-model="dialogDelete" max-width="500px">
-              <v-card>
-                <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-                  <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
-                  <v-spacer></v-spacer>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-          </v-toolbar>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <v-dialog v-model="dialogDelete" max-width="500px">
+            <v-card>
+              <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
         </template>
         <template v-slot:item.actions="{ item }">
           <v-icon @click="paidFee(item)" color="success">mdi-check</v-icon>
@@ -49,20 +51,6 @@ export default {
       { text: "Email", value: "email", sortable: false },
       { text: "Date ", value: "date", sortable: true },
       { text: "Actions", value: "actions", sortable: false }
-    ],
-    newRegistedUser: [
-      {
-        fullname: "Abebe Zegeye",
-        phonenumber: "+25121314159",
-        email: "abebe@gmail.com",
-        date: "24-Tus-2020"
-      },
-      {
-        fullname: "Abebe Zegeye",
-        phonenumber: "+25121314159",
-        email: "abebe@gmail.com",
-        date: "22-Tus-2020"
-      }
     ]
   }),
 
