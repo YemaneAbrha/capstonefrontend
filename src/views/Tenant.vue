3e23a5b23240e64eeaa1f0fc6a83486034fc3cb1
YemaneAbrha <yemaneabrha21@gmail.com>
2021-05-21 10:21:39 +0300

    Typo

--- src/views/Tenant.vue
+++ src/views/Tenant.vue
@@ -21,15 +21,15 @@
               <v-card-actions>
                 <v-spacer></v-spacer>
                 <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-                <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                <v-btn color="blue darken-1" text @click="declineTenantConfirm">OK</v-btn>
                 <v-spacer></v-spacer>
               </v-card-actions>
             </v-card>
           </v-dialog>
         </template>
         <template v-slot:item.actions="{ item }">
-          <v-icon @click="paidFee(item)" color="success">mdi-check</v-icon>
-          <v-icon small @click="deleteItem(item)" color="error">mdi-delete</v-icon>
+          <v-icon @click="approveTenant(item)" color="success">mdi-check</v-icon>
+          <v-icon small @click="declineTenant(item)" color="error">mdi-delete</v-icon>
         </template>
         <template v-slot:no-data>No Data</template>
       </v-data-table>
@@ -55,48 +55,45 @@ export default {
   }),
 
   watch: {
-    dialog(val) {
-      val || this.close();
-    },
     dialogDelete(val) {
       val || this.closeDelete();
     }
   },
   methods: {
-    paidFee(item) {
+    approveTenant(item) {
       this.editedIndex = this.$store.state.pendingBill.indexOf(item);
       console.log(item);
-      this.$store.commit("addPaidFee", {
+      this.$store.commit("approveTenant", {
         item: item,
         index: this.editedIndex
       });
     },
-    deleteItem(item) {
-      this.editedIndex = this.$store.state.pendingBill.indexOf(item);
+    declineTenant(item) {
+      // console.log(
+      //   "sdjksddsjds djsdkjaskdjaksjdkasjdas _____________________------"
+      // );
+      this.editedIndex = this.$store.state.newRegisteredTenant.indexOf(item);
+      console.log(item);
       this.editedItem = Object.assign({}, item);
       this.dialogDelete = true;
     },
 
-    deleteItemConfirm() {
-      this.$store.commit("deletePendingBill", {
+    declineTenantConfirm() {
+      console.log("Haha");
+
+      this.$store.commit("declineTenant", {
         index: this.editedIndex
       });
+
+      console.log(this.editedIndex);
+
       this.closeDelete();
     },
-
-    close() {
-      // this.dialog = false;
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      });
-    },
-
     closeDelete() {
       this.dialogDelete = false;
       this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
+        // this.editedItem = Object.assign({}, this.defaultItem);
+        // this.editedIndex = -1;
       });
     },
 
