7c04f6f54e70956282447d4d67af424174112027
YemaneAbrha <yemaneabrha21@gmail.com>
2021-05-21 15:41:48 +0300

    Sample Data Intialized

---
+++ src/data/lease.js
@@ -0,0 +1,60 @@
+var occupied = [
+  {
+    unitName: "M01",
+    floorNumber: "Mizanen",
+    totalArea: "40",
+    billableArea: "30",
+    rentalRate: "150",
+    tenant: "Abebe Zegeye",
+    paymentPeriod: "Monthly",
+  },
+
+  {
+    unitName: "M01",
+    floorNumber: "Mizanen",
+    totalArea: "40",
+    billableArea: "30",
+    rentalRate: "150",
+    tenant: "Abebe Zegeye",
+    paymentPeriod: "Monthly",
+  },
+  {
+    unitName: "M01",
+    floorNumber: "Mizanen",
+    totalArea: "40",
+    billableArea: "30",
+    rentalRate: "150",
+    tenant: "Abebe Zegeye",
+    paymentPeriod: "Monthly",
+  },
+  {
+    unitName: "M01",
+    floorNumber: "Mizanen",
+    totalArea: "40",
+    billableArea: "30",
+    rentalRate: "150",
+    tenant: "Abebe Zegeye",
+    paymentPeriod: "Monthly",
+  },
+  {
+    unitName: "M01",
+    floorNumber: "Mizanen",
+    totalArea: "40",
+    billableArea: "30",
+    rentalRate: "150",
+    tenant: "Abebe Zegeye",
+    paymentPeriod: "Monthly",
+  },
+  {
+    unitName: "M01",
+    floorNumber: "Mizanen",
+    totalArea: "40",
+    billableArea: "30",
+    rentalRate: "150",
+    tenant: "Abebe Zegeye",
+    paymentPeriod: "Monthly",
+  },
+];
+
+var available = [];
+export { occupied, available };
