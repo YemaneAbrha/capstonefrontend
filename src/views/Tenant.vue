a1ed74e43482cc4084617be6032c315d28b137fe
YemaneAbrha <yemaneabrha21@gmail.com>
2021-05-21 13:28:01 +0300

    Typo

--- src/views/Tenant.vue
+++ src/views/Tenant.vue
@@ -1,108 +1,19 @@
 <template>
-  <v-container>
-    <v-row align="center" justify="center">
-      <v-container ma-5>
-        <h3 class="text-center display-3 hidden-xs-only">New Registered Tenant</h3>
-        <h3 class="text-center display-1 hidden-sm-and-up">New Registered Tenant</h3>
-      </v-container>
-    </v-row>
-    <v-row align="center" justify="center">
-      <v-data-table
-        :headers="headers"
-        :items="$store.state.newRegisteredTenant"
-        class="elevation-1"
-      >
-        <template v-slot:top>
-          <v-divider class="mx-4" inset vertical></v-divider>
-          <v-spacer></v-spacer>
-          <v-dialog v-model="dialogDelete" max-width="500px">
-            <v-card>
-              <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-                <v-btn color="blue darken-1" text @click="declineTenantConfirm">OK</v-btn>
-                <v-spacer></v-spacer>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </template>
-        <template v-slot:item.actions="{ item }">
-          <v-icon @click="approveTenant(item)" color="success">mdi-check</v-icon>
-          <v-icon small @click="declineTenant(item)" color="error">mdi-delete</v-icon>
-        </template>
-        <template v-slot:no-data>No Data</template>
-      </v-data-table>
-    </v-row>
-  </v-container>
+  <v-sheet>
+    <v-container class="text-center" pa-15>
+      <v-row></v-row>
+    </v-container>
+    <NewRegisteredTenant />
+    <Tenant />
+  </v-sheet>
 </template>
 <script>
+import NewRegisteredTenant from "../components/tenant/NewTenantTable";
+import Tenant from "../components/tenant/TenantTable";
 export default {
-  data: () => ({
-    dialogDelete: false,
-    headers: [
-      {
-        text: "Full Name",
-        align: "start",
-        sortable: false,
-        value: "fullname"
-      },
-      { text: "Phone Number", value: "phonenumber", sortable: false },
-      { text: "Email", value: "email", sortable: false },
-      { text: "Date ", value: "date", sortable: true },
-      { text: "Actions", value: "actions", sortable: false }
-    ]
-  }),
-
-  watch: {
-    dialogDelete(val) {
-      val || this.closeDelete();
-    }
-  },
-  methods: {
-    approveTenant(item) {
-      this.editedIndex = this.$store.state.pendingBill.indexOf(item);
-      console.log(item);
-      this.$store.commit("approveTenant", {
-        item: item,
-        index: this.editedIndex
-      });
-    },
-    declineTenant(item) {
-      // console.log(
-      //   "sdjksddsjds djsdkjaskdjaksjdkasjdas _____________________------"
-      // );
-      this.editedIndex = this.$store.state.newRegisteredTenant.indexOf(item);
-      console.log(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialogDelete = true;
-    },
-
-    declineTenantConfirm() {
-      console.log("Haha");
-
-      this.$store.commit("declineTenant", {
-        index: this.editedIndex
-      });
-
-      console.log(this.editedIndex);
-
-      this.closeDelete();
-    },
-    closeDelete() {
-      this.dialogDelete = false;
-      this.$nextTick(() => {
-        // this.editedItem = Object.assign({}, this.defaultItem);
-        // this.editedIndex = -1;
-      });
-    },
-
-    save() {
-      this.$store.commit("editBill", {
-        item: this.editedItem
-      });
-      this.close();
-    }
+  components: {
+    NewRegisteredTenant,
+    Tenant
   }
 };
 </script>
