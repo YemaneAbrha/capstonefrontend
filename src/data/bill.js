eb7f43c5197996b76c40af6ef1cdc5b786a85eac
YemaneAbrha <yemaneabrha21@gmail.com>
2021-05-21 09:26:25 +0300

    Comment Removed

--- src/data/bill.js
+++ src/data/bill.js
@@ -1,58 +1,3 @@
-// export default [
-//   ((pendingBill = [
-//     {
-//       title: "Frozen Yogurt",
-//       amount: 159,
-//       description: "Here is the description for the consto mosti ",
-//       dueDate: "24-Tus-2020",
-//     },
-//     {
-//       title: "Ice cream sandwich",
-//       amount: 237,
-//       description: "Here is the description for the consto mosti ",
-//       dueDate: "23-Tus-2031",
-//     },
-//     {
-//       title: "Eclair",
-//       amount: 262,
-//       description: "Here is the description for the consto mosti ",
-//       dueDate: "23-Tus-2031",
-//     },
-//     {
-//       title: "Cupcake",
-//       amount: 305,
-//       description: '"Here is the description for the consto mosti ",',
-//       dueDate: "23-Tus-2031",
-//     },
-//   ]),
-//   (lateFees = [
-//     {
-//       title: "Frozen Yogurt",
-//       amount: 159,
-//       description: "Here is the description for the consto mosti ",
-//       dueDate: "24-Tus-2020",
-//     },
-//     {
-//       title: "Ice cream sandwich",
-//       amount: 237,
-//       description: "Here is the description for the consto mosti ",
-//       dueDate: "23-Tus-2031",
-//     },
-//     {
-//       title: "Eclair",
-//       amount: 262,
-//       description: "Here is the description for the consto mosti ",
-//       dueDate: "23-Tus-2031",
-//     },
-//     {
-//       title: "Cupcake",
-//       amount: 305,
-//       description: '"Here is the description for the consto mosti ",',
-//       dueDate: "23-Tus-2031",
-//     },
-//   ])),
-// ];
-
 var pendingBill = [
   {
     id: "1",
@@ -112,6 +57,5 @@ var paidFee = [
     dueDate: "2019-23-99",
   },
 ];
-// console.log("Yay");
-// console.log(bill);
+
 export { pendingBill, lateFee, paidFee };
