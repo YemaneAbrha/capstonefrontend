efb7e3ae5490d6c7708c66840246798e6af188fd
YemaneAbrha <yemaneabrha21@gmail.com>
2021-05-21 09:29:56 +0300

    Tenant Sample Data Created

---
+++ src/data/tenant.js
@@ -0,0 +1,29 @@
+var newRegisteredTenant = [
+  {
+    fullname: "Abebe Zegeye",
+    phonenumber: "+25121314159",
+    email: "abebe@gmail.com",
+    date: "24-Tus-2020",
+  },
+  {
+    fullname: "Abebe Zegeye",
+    phonenumber: "+25121314159",
+    email: "abebe@gmail.com",
+    date: "22-Tus-2020",
+  },
+];
+var tenant = [
+  {
+    fullname: "Amare Zeleke",
+    phonenumber: "+25121314159",
+    email: "mob@gmail.com",
+    date: "23-Tus-2020",
+  },
+  {
+    fullname: "Zebib Alula",
+    phonenumber: "+25121314159",
+    email: "sher@gmail.com",
+    date: "22-Tus-2020",
+  },
+];
+export { newRegisteredTenant, tenant };
